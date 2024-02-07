b57958f5a765a0e5957b7536f18e2dab43cce444
knork-fork <looka1998@gmail.com>
2024-02-08 00:09:30 +0100

    Setup a test helper service

--- src/Controller/Test/TestController.php
+++ src/Controller/Test/TestController.php
@@ -3,16 +3,21 @@ declare(strict_types=1);
 
 namespace App\Controller\Test;
 
+use App\Service\Test\TestService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 final class TestController extends AbstractController
 {
+    public function __construct(
+        private TestService $testService
+    ) {
+    }
+
     public function test(): JsonResponse
     {
-        return new JsonResponse([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/Test/TestController.php',
-        ]);
+        return new JsonResponse(
+            $this->testService->test()
+        );
     }
 }
